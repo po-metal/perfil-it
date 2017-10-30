fd29a5ffc08adc6d1a4697ab8064f32d3d8081ed
cristian <cristian@192.168.5.112>
2017-10-30 18:22:21 -0300

    Mixim

--- vue/pi/src/components/cv-education.vue
+++ vue/pi/src/components/cv-education.vue
@@ -10,34 +10,18 @@
       </ul>
       <modal :modalId="modalId" :title="modalTitle" :isSaved="isSaved">
           <div class="row">
-              <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 text-center">
-                  <form method="post" name="CvPicture" id="CvPicture" class="form-horizontal" role="form"
-                        v-on:submit.prevent="save" enctype="multipart/form-data">
-                      <div class="col-lg-12 col-md-12 col-xs-12">
-                          <div class="form-group">
-                              <label class="control-label"></label>
-                              <label class="btn btn-default fa fa-upload">
-                                  <input type="file" name="picture" id="picture" class="hidden" @change="save">
-                              </label>
-                          </div>
-                      </div>
-                  </form>
-              </div>
-              <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 text-center">
-                  <div class="progress progress-striped active margin6">
-                      <div id="cv-picture-bar" class="progress-bar progress-bar-warning " role="progressbar"
-                           :aria-valuenow="progressBar" :style="{width: progressBar + '%' }" aria-valuemin="1" aria-valuemax="100">
-                      </div>
-                  </div>
-              </div>
+
           </div>
       </modal>
   </div>
 </template>
 
 <script>
+  import crud from './utils/crud'
+
 export default {
   name: 'cv-education',
+  mixins: [crud],
   data () {
     return {
       loading: false,
@@ -46,54 +30,32 @@ export default {
       modalId: 'modal-cv-education',
       modalTitle: 'Educación',
       education: '',
-      state: ''
+      educationState: '',
+      title: '',
+      state: '',
+      url: {
+        get:  '/pi/cv-education/get',
+        save: '/pi/cv-education/save'
+      }
     }
   },
   methods: {
-    unsaved: function () {
-      this.isSaved = false
-    },
-    save: function () {
-      this.errors = ''
-      this.submitInProgress = true
-      axios.post(
-        '/pi/cv-education/save', qs.stringify({
-          education: this.education,
-          educationState: this.educationState,
-          title: this.title
-        })
-      ).then((response) => {
-        if (response.data.status) {
-          this.isSaved = true
-          this.populate(response.data.data)
-        } else {
-          this.errors = response.data.errors
-        }
-        this.submitInProgress = false
-      }).catch((error) => {
-        this.errors = error.response.data.errors
-        this.submitInProgress = false
-      })
-    },
-    loadProps: function () {
-      axios.get(
-        '/pi/cv-education/get',
-        {
-          headers: {
-            accept: 'application/json'
-          }
-        })
-        .then((response) => {
-          this.populate(response.data)
-          this.loading = true
-        })
-    },
     populate: function (data) {
       this.education = data.education
-      this.state = data.state
+      this.educationState = data.educationState
+      this.title = data.title
     },
     mounted: function () {
       this.loadProps()
+    }
+  },
+  computed: {
+    postParams: function(){
+      return {
+        education: this.education,
+        educationState: this.educationState,
+        title: this.title
+      }
     }
   }
 
